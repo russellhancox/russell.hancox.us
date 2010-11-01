9b3d3a12cbb654076ae74ccd15d67614d678aaef
Russell Hancox <admin@soulsniper.net>
2010-11-01 17:49:53 +0000

    Make page title appear in browser title, make page-specific JS a bit smarter

--- code.js
+++ code.js
@@ -1,10 +1,13 @@
 function startup()
 {
-	// Call the function for the correct page
-	page_id();
+	// Call the function for the correct page, if it's defined
+	if (typeof page_id == 'function')
+	{
+		page_id();
+	}
 }
 
-function index()
+function About()
 {
 	// Calculate my current age and stick it in the right place
 	var today = new Date(); 
@@ -12,6 +15,3 @@ function index()
 	if (10 > today.getMonth() || (10 == today.getMonth() && today.getDate() < 25)) { age = age - 1; }
 	document.getElementById('myage').innerHTML = age + " year old ";
 }
-
-function blog()
-{}	
